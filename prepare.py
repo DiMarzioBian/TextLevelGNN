934bfd3fa49f241a6fc2f76c5bac5cf9a873bbe9
DiMarzio Bian <326361233@qq.com>
2022-04-20 03:33:53 +0800

    updated

--- prepare.py
+++ prepare.py
@@ -1,221 +1,127 @@
 import argparse
-import os
-import time
-import pickle
-import codecs
 import numpy as np
-import pandas as pd
-from collections import Counter
-from nltk.tokenize import TweetTokenizer
-import multiprocessing as mp
+import codecs
+import pickle
+import time
+import zipfile
 
 
 def main():
-    parser = argparse.ArgumentParser(description='TextLevelGNN data preprocess script')
-    parser.add_argument('--dataset', type=str, default='R8', choices=['mr', 'ohsumed', 'R8', 'R52'],
-                        help='name of dataset used')
-    parser.add_argument('--path_data', type=str, default='./data/',
-                        help='path of the data corpus')
-    parser.add_argument('--max_len_text', type=int, default=100,
-                        help='maximum length of text')
-    parser.add_argument('--n_word_min', type=int, default=2,
-                        help='minimum word counts')
+    parser = argparse.ArgumentParser(description='TextLevelGNN-DGL data packaging project')
 
-    parser.add_argument('--preserve_case', type=bool, default=False,
-                        help='preserve words case')
-    parser.add_argument('--pad_num', type=bool, default=True,
-                        help='pad all numbers to a same <num>')
+    # experiment setting
+    parser.add_argument('--dataset', type=str, default='ohsumed', choices=['r8', 'r52', 'ohsumed'], help='used dataset')
+    parser.add_argument('--pretrained', type=bool, default=True, help='use pretrained GloVe embeddings')
+    parser.add_argument('--d_pretrained', type=int, default=300, help='pretrained embedding dimension')
+    parser.add_argument('--seed', type=int, default=1111, help='random seed')
 
-    parser.add_argument('--pretrained', type=bool, default=True,
-                        help='use _pretrained GloVe_300d')
-    parser.add_argument('--d_pretrained', type=int, default=300,
-                        help='pretrained embedding dimension')
-    parser.add_argument('--path_pretrained', type=str, default='./data/glove.6B.300d.txt',
-                        help='path of pretrained GloVe embeddings')
-    parser.add_argument('--seed', type=int, default=1111,
-                        help='random seed')
+    # path settings
+    parser.add_argument('--path_data', type=str, default='./data/', help='path of the data corpus')
 
     args = parser.parse_args()
-    if args.dataset == 'ohsumed':
-        args.dataset = 'ohsumed_single_23'
-        args.n_word_min = 3
+    np.random.seed(args.seed)
 
-    if not os.path.exists(args.path_data + args.dataset):
-        raise FileNotFoundError('Dataset "{data}" raw corpus file not found.'.format(data=args.dataset))
+    if args.dataset not in ['r8', 'r52', 'ohsumed']:
+        raise ValueError('Data {data} not supported, currently supports "r8", "r52" and "ohsumed".')
 
-    # data preprocess
+    # read files
     print('\n[info] Dataset:', args.dataset)
     time_start = time.time()
 
-    tr_data, te_data, tr_gt, te_gt = read_corpus(args)
-    tr_tokens, te_tokens, tr_gt, te_gt = tokenize(args, tr_data, te_data, tr_gt, te_gt)
-
-    x, y, word2idx = extract_feature(tr_processed, te_processed, n_word_min=args.n_word_min)
+    label2idx = read_label(args.path_data + args.dataset + '/label.txt')
+    word2idx = read_vocab(args.path_data + args.dataset + '/vocab-5.txt')
+    args.n_class = len(label2idx)
+    args.n_word = len(word2idx)
+    print('\tTotal classes:', args.n_class)
+    print('\tTotal words:', args.n_word)
 
     embeds = get_embedding(args, word2idx)
 
+    tr_data, tr_gt = read_corpus(args.path_data + args.dataset + '/train-stemmed.txt', label2idx, word2idx)
+    print('\n\tTotal training samples:', len(tr_data))
+
+    val_data, val_gt = read_corpus(args.path_data + args.dataset + '/valid-stemmed.txt', label2idx, word2idx)
+    print('\tTotal validation samples:', len(val_data))
+
+    te_data, te_gt = read_corpus(args.path_data + args.dataset + '/test-stemmed.txt', label2idx, word2idx)
+    print('\tTotal testing samples:', len(te_data))
+
     # save processed data
-    n_train = df_data.subset.value_counts()['train']
     mappings = {
-        'tr_data': x[:n_train],
-        'tr_gt': y[:n_train],
-        'te_data': x[n_train:],
-        'te_gt': y[n_train:],
+        'label2idx': label2idx,
         'word2idx': word2idx,
+        'tr_data': tr_data,
+        'tr_gt': tr_gt,
+        'val_data': val_data,
+        'val_gt': val_gt,
+        'te_data': te_data,
+        'te_gt': te_gt,
         'embeds': embeds,
         'args': args
     }
+
     with open(args.path_data + args.dataset + '.pkl', 'wb') as f:
         pickle.dump(mappings, f)
+    with zipfile.ZipFile(args.path_data + args.dataset + '.zip', 'w') as zf:
+        zf.write(args.path_data + args.dataset + '.pkl', compress_type=zipfile.ZIP_DEFLATED)
 
     print('\n[info] Time consumed: {:.2f}s'.format(time.time() - time_start))
 
 
-def read_corpus(args):
-    """ Export data from corpus file """
+def read_label(path):
+    """ Extract and encode labels. """
+    with open(path) as f:
+        labels = f.read().split('\n')
 
-    path_data, dataset = args.path_data + args.dataset, args.dataset
-    tr_data, te_data, tr_gt, te_gt = [], [], [], []
-
-    # R20
-    if dataset == 'R8' or dataset == 'R52':
-        with open(os.path.join(path_data, 'train.txt')) as f:
-            lines = f.readlines()
-
-        for line in lines:
-            label, text = line.strip().split('\t')
-            tr_data.append([text])
-            tr_gt.append([label])
-
-        with open(os.path.join(path_data, 'test.txt')) as f:
-            lines = f.readlines()
-        for line in lines:
-            label, text = line.strip().split('\t')
-            te_data.append([text])
-            te_gt.append([label])
-
-    # ohsumed_single_23
-    elif dataset == 'ohsumed_single_23':
-
-        path_data_subset = os.path.join(path_data, 'training')
-        for label in os.listdir(path_data_subset):
-            for doc in os.listdir(os.path.join(path_data_subset, label)):
-                with open(os.path.join(path_data_subset, label, doc)) as f:
-                    lines = f.readlines()
-                text = " ".join([line.strip() for line in lines])
-                tr_data.append([text])
-                tr_gt.append([label])
-
-        path_data_subset = os.path.join(path_data, 'test')
-        for label in os.listdir(path_data_subset):
-            for doc in os.listdir(os.path.join(path_data_subset, label)):
-                with open(os.path.join(path_data_subset, label, doc)) as f:
-                    lines = f.readlines()
-                text = " ".join([line.strip() for line in lines])
-                te_data.append([text])
-                te_gt.append([label])
-
-    # movie review
-    elif dataset == 'mr':
-
-        text_data_path = os.path.join(path_data, 'text_train.txt')
-        with open(text_data_path, 'rb') as f:
-            text_lines = f.readlines()
-        label_data_path = os.path.join(path_data, 'label_train.txt')
-        with open(label_data_path, 'rb') as f:
-            label_lines = f.readlines()
-        for (text, label) in zip(text_lines, label_lines):
-            tr_data += [str(text.strip())]
-            tr_gt += [str(label.strip())]
-
-        text_data_path = os.path.join(path_data, 'text_test.txt')
-        with open(text_data_path, 'rb') as f:
-            text_lines = f.readlines()
-        label_data_path = os.path.join(path_data, 'label_test.txt')
-        with open(label_data_path, 'rb') as f:
-            label_lines = f.readlines()
-        for (text, label) in zip(text_lines, label_lines):
-            te_data += [str(text.strip())]
-            te_gt += [str(label.strip())]
-
-    else:
-        raise FileNotFoundError('Dataset not supported.')
-
-    return tr_data, te_data, tr_gt, te_gt
+    return {label: i for i, label in enumerate(labels)}
 
 
-def tokenize(args, tr_data, te_data, tr_gt, te_gt):
-    """Tokenize and filter dataset text. """
+def read_vocab(path):
+    """ Extract words from vocab and encode. """
+    with open(path) as f:
+        words = f.read().split('\n')
+    word2idx = {word: i + 1 for i, word in enumerate(words)}
+    word2idx['<pad>'] = 0
 
-    tokenizer = TweetTokenizer(preserve_case=args.preserve_case)
-
-    tr_tokens, te_tokens, tr_gt_tokens, te_gt_tokens = [], [], [], []
-    dict_gt = {}
-
-    for label, text in tr_data:
-
-
-
-    [w if (not w.isdigit() and not isinstance(w, float)) or not pad_num else '<num>'
-     for w in tokenizer.tokenize(text)]
-
-    df_data = df_data[df_data.tokens.apply(lambda tokens: (len(tokens) > 0))]  # remove empty sentences
-    print('\n\tMax sentence length:', args.max_len_text)
-    print('\tTrain samples: {}, test samples: {}'
-          .format(len(df_data[df_data.subset == 'train']), len(df_data[df_data.subset == 'test'])))
-
-    return df_data
-
-
-
-def extract_feature(tr_data, te_data, n_word_min):
-    """ Preprocess word to index, and encode labels. """
-
-    word2idx = get_word2idx(df_data.tokens.to_list(), n_word_min=n_word_min)
-    x = transform_word2idx_mp(df_data.tokens.to_list(), word2idx=word2idx)
-    y = pd.Categorical(df_data.label).codes
-
-    return x, y, word2idx
-
-
-def get_word2idx(text, n_word_min):
-    """ Map words into indices."""
-
-    word_counts = Counter([w for tokens in text for w in tokens])
-    vocab = [w for w, v in word_counts.items() if v > n_word_min]
-
-    word2idx = {word: i + 1 for i, word in enumerate(vocab)}
-    word2idx['<pad>'], word2idx['<unk>'] = 0, len(vocab) + 1
-
-    print('\tMost common words:', word_counts.most_common(10))
-    print('\tTotal words:', len(vocab))
     return word2idx
 
 
-def transform_word2idx_mp(tokens_of_texts, word2idx):
-    with mp.Pool(mp.cpu_count()) as p:
-        return p.starmap(transform_word2idx, map(lambda tokens: (tokens, word2idx), tokens_of_texts))
+def read_corpus(path, label2idx, word2idx):
+    """ Encode both corpus and labels. """
+    with open(path) as f:
+        content = [line.split('\t') for line in f.read().split('\n')]
+
+    data = [[encode_word(word, word2idx) for word in x[1].split()] for x in content]
+    gt = [label2idx[x[0]] for x in content]
+    return data, gt
 
 
-def transform_word2idx(tokens, word2idx):
-    """ Convert word token to index. """
-
-    unk_idx = len(word2idx) - 1
-    return [word2idx.get(w) if word2idx.get(w) else unk_idx for w in tokens]
+def encode_word(word, word2idx):
+    """ Encode word considering unknown word. """
+    try:
+        idx = word2idx[word]
+    except KeyError:
+        idx = word2idx['UNK']
+    return idx
 
 
 def get_embedding(args, word2idx):
-    """ Find words in pretrained GloVe embeddings."""
-
-    embeds_word = np.random.uniform(-np.sqrt(0.06), np.sqrt(0.06), (len(word2idx), args.d_pretrained))  # initializing
-    emb_counts = 0
-
+    """ Find words in pretrained GloVe embeddings. """
     if args.pretrained:
-        for i, line in enumerate(codecs.open(args.path_pretrained, 'r', 'utf-8')):
+        path = args.path_data + 'glove.6B.' + str(args.d_pretrained) + 'd.txt'
+        embeds_word = np.random.uniform(-np.sqrt(0.06), np.sqrt(0.06), (len(word2idx), args.d_pretrained))
+        emb_counts = 0
+        for i, line in enumerate(codecs.open(path, 'r', 'utf-8')):
             s = line.strip().split()
             if len(s) == (args.d_pretrained + 1) and s[0] in word2idx:
                 embeds_word[word2idx[s[0]]] = np.array([float(i) for i in s[1:]])
                 emb_counts += 1
+
+        embeds_word[0] = np.zeros_like(embeds_word[0])  # <pad>
+
+    else:
+        embeds_word = None
+        emb_counts = 'disabled pretrained'
 
     print('\tPretrained GloVe found:', emb_counts)
     return embeds_word
